96488226dbf8bf837f3a0b3c293c859dc587cea9
stanislaus-onwuka <kachi2505@yahoo.com>
2022-12-30 11:35:07 +0100

    Contact page in progress

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -9,12 +9,35 @@ export default function Contact() {
             title="Contact me"
             showSubFooter={false}
             isNavDark={false}
-            invertMenuColours={true}
+            invertMenuColours={false}
             spinningLogoClassName="bottom-12 left-[72px]"
         >
             <main className="bg-secondary pt-[111px]">
                 <section className="h-screen">
+                    <div>
+                        <div>
+                            <div>
+                                <p className="text-[96px] leading-[131px] text-primary uppercase font-body font-semibold">
+                                    say hello
+                                </p>
+                                <p className="text-[96px] leading-[131px] text-primary uppercase font-body font-semibold">
+                                    to Femi Johnsn -
+                                </p>
+                            </div>
+                            <a href="#">
+                                
+                            </a>
+                        </div>
+                        <a href="#" className="text-[96px] leading-[131px] text-primary uppercase font-body font-semibold">
+                            femijohnsnn@gmail.com
+                        </a>
+                    </div>
+                    <div>
 
+                    </div>
+                </section>
+                <section>
+                    
                 </section>
             </main>
         </DefaultLayout>
--- pages/photography/all/index.js
+++ pages/photography/all/index.js
@@ -1,17 +1,65 @@
 import DefaultLayout from "../../../components/layouts/default-layout";
 import styles from '../../../styles/pages/AllPhotos.module.scss'
+import Link from "next/link";
+import { photosPreview } from "../../../utils/constants";
+import PreviewLink from "../../../components/photography/preview-link";
+import { classNames } from "../../../utils/classNames";
 
 export default function AllPhotos() {
+
     return (
         <DefaultLayout
             className={styles["container"]}
             title="All Photos"
             showSubFooter={true}
             isNavDark={true}
+            showBackButton={true}
             spinningLogoClassName="hidden"
         >
-            <h1 className="text-heading-2 text-secondary text-center">In progress</h1>
-
+            <main className="flex mt-[111px] mx-[9%]">
+                <section className="font-body fixed top-[35%]">
+                    <h3
+                        className="text-base-semi-bold uppercase text-secondary mb-4"
+                    >
+                        Filter by
+                    </h3>
+                    <ul>
+                        <li
+                            className="text-base-semi-bold uppercase text-secondary p-2 mb-1"
+                        >
+                            <Link href="/photography/all?type=indoor">
+                                Indoor
+                            </Link>
+                        </li>
+                        <li
+                            className="text-base-semi-bold uppercase text-secondary p-2"
+                        >
+                            <Link href="/photography/all?type=outdoor">
+                                Outdoor
+                            </Link>
+                        </li>
+                    </ul>
+                </section>
+                <section className="ml-[400px]">
+                    <h4 className="text-display-4 font-cursive mb-14 text-secondary">
+                        PRODIGY
+                    </h4>
+                    <div className={classNames("grid gap-8", styles["preview-grid"])}>
+                        {
+                            photosPreview.map((pic) => {
+                                if (pic.isLink) {
+                                    return (
+                                        <PreviewLink
+                                            key={pic.id}
+                                            picData={pic}
+                                        />
+                                    )
+                                }
+                            })
+                        }
+                    </div>
+                </section>
+            </main>
         </DefaultLayout>
     )
 }
