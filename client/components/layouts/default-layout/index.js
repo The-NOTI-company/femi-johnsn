e5ecb9f0d6a1aa118fd7d2f1cb2f9645b0654870
stanislaus-onwuka <kachi2505@yahoo.com>
2023-07-16 08:04:26 +0100

    Animation fix

--- client/components/layouts/default-layout/index.js
+++ client/components/layouts/default-layout/index.js
@@ -3,6 +3,7 @@ import Footer from "../../footer";
 import SeoHead from "../../seo-head";
 import SpinningLogo from "../../spinning-logo";
 import { classNames } from "../../../utils/classNames";
+import AnimationLayer from "../../animation-layer";
 
 export default function DefaultLayout({ className, title, description, children, showSubFooter, isNavDark, backButtonDestination, invertMenuColours, setMixBlend, spinningLogoClassName }){
     return (
@@ -13,15 +14,17 @@ export default function DefaultLayout({ className, title, description, children,
                     description={description}
                 />
                 <div className="mx-auto w-full">
-                    <Navigation
-                        backButtonDestination={backButtonDestination}
-                        isNavDark={isNavDark}
-                        invertMenuColours={invertMenuColours}
-                        setMixBlend={setMixBlend}
-                    />
-                    <>
-                        {children}
-                    </>
+                    <AnimationLayer>
+                        <Navigation
+                            backButtonDestination={backButtonDestination}
+                            isNavDark={isNavDark}
+                            invertMenuColours={invertMenuColours}
+                            setMixBlend={setMixBlend}
+                        />
+                        <>
+                            {children}
+                        </>
+                    </AnimationLayer> 
                     <SpinningLogo className={spinningLogoClassName} />
                     <a href="https://www.designnominees.com/sites/femi-johnsn" title="Design Nominees" style={{width:'70px', height:'130px', position:'fixed', top:'150px', right:0, zIndex:99999, textIndent:-'9999px', background: `url(${'http://www.designnominees.com/ribbons/designnominees-ribbon-sotd-white-right.png'}) no-repeat` }} target="_blank" rel="noreferrer"></a>
                     <Footer
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast'
 import '../styles/globals.css'
 import { useRouter } from "next/router";
@@ -6,44 +6,8 @@ import { useRouter } from "next/router";
 import * as gtag from "../utils/gtag";
 import Script from "next/script";
 
-import { motion } from 'framer-motion';
 import {LenisProvider} from '../context/lenisContext';
 
-
-const AnimationLayout = ({ children }) => {
-  const [isAnimating, setIsAnimating] = useState(false);
-  const router = useRouter();
-  
-  useEffect(() => {
-    const handleRouteChangeStart = () => {
-      setIsAnimating(true);
-    };
-
-    const handleRouteChangeComplete = () => {
-      setIsAnimating(false);
-    };
-
-    router.events.on('routeChangeStart', handleRouteChangeStart);
-    router.events.on('routeChangeComplete', handleRouteChangeComplete);
-
-    return () => {
-      router.events.off('routeChangeStart', handleRouteChangeStart);
-      router.events.off('routeChangeComplete', handleRouteChangeComplete);
-    };
-  }, [router.events]);
-
-  return (
-    <motion.div
-      key={router.asPath}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: isAnimating ? 0 : 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.3, delay: 0.03 }}
-    >
-      {children}
-    </motion.div>
-  )
-}
 
 
 function MyApp({ Component, pageProps }) {
@@ -86,9 +50,7 @@ function MyApp({ Component, pageProps }) {
       </>
       <Toaster />
       <LenisProvider>
-        <AnimationLayout>
-          <Component {...pageProps} />
-        </AnimationLayout>
+        <Component {...pageProps} />
       </LenisProvider>
     </>
   )
