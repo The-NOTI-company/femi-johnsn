24fe45b789163da18041ae79870ea99af1d0a4c7
stanislaus-onwuka <kachi2505@yahoo.com>
2024-05-04 13:40:49 +0100

    CMS setup

---
+++ api/src/api/collection-preview/routes/collection-preview.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * collection-preview router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+
+module.exports = createCoreRouter('api::collection-preview.collection-preview')
---
+++ api/src/api/collection-preview/services/collection-preview.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * collection-preview service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::collection-preview.collection-preview');
